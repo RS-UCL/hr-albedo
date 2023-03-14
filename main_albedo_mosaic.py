1479b4316f1f0c38a78788e485f5942a311881d4
ruisong <rui.song@ucl.ac.uk>
2023-03-14 12:42:04 +0000

    st s2gm

--- main_albedo_mosaic.py
+++ main_albedo_mosaic.py
@@ -69,6 +69,7 @@ def get_modis_jasmin(modis_tile, sentinel2_directory):
 
 from cal_endmember_mosaic import *
 from find_mosaic_mcd43 import *
+import datetime
 import yaml
 
 S2GM_config = LoadConfig('./config_mosaic.yaml')
