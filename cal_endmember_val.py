c3e838b20a5c17ec40aa69fdb68455de4f62181d
ruisong <rui.song@ucl.ac.uk>
2023-03-24 19:02:56 +0000

    st s2gm

--- cal_endmember_val.py
+++ cal_endmember_val.py
@@ -137,7 +137,8 @@ def cal_endmember(sentinel2_directory):
     file_subdirectory = os.path.join(file_subdirectory, os.listdir(file_subdirectory)[0])
     file_subdirectory = os.path.join(file_subdirectory, 'IMG_DATA')
 
-    print(file_subdirectory)
+    kernel_weights = np.load(file_subdirectory + '/kernel_weights.npy')
+    print(kernel_weights)
     quit()
     modis_band001_file = 'HDF4_EOS:EOS_GRID:"%s":MOD_Grid_BRDF:BRDF_Albedo_Parameters_Band1' % mcd43a1_file
     modis_band002_file = 'HDF4_EOS:EOS_GRID:"%s":MOD_Grid_BRDF:BRDF_Albedo_Parameters_Band2' % mcd43a1_file
