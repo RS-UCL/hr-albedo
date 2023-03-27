a1817fca8b9e36d90824270ad91f03ff97299542
ruisong <rui.song@ucl.ac.uk>
2023-03-27 23:06:26 +0100

    val

--- cal_endmember_val.py
+++ cal_endmember_val.py
@@ -373,21 +373,21 @@ def cal_endmember(sentinel2_directory):
     # CalAbundanceMap = FCLS()
     # print("-----------> Start calculating abundance on aggregated S2 scence.\n")
     # s2_abundance_500m = CalAbundanceMap.map(s2_resampled_matrix_filtered_interp_500m, main_endmember)
-    for k in range(s2_abundance_500m.shape[2]):
-        s2_abundance_500m[:, :, k][boa_band02_500m_array < 0] = np.nan
-        s2_abundance_500m[:, :, k][boa_mask_500m_array > 0.] = np.nan
+    # for k in range(s2_abundance_500m.shape[2]):
+    #     s2_abundance_500m[:, :, k][boa_band02_500m_array < 0] = np.nan
+    #     s2_abundance_500m[:, :, k][boa_mask_500m_array > 0.] = np.nan
     #
-    print("-----------> Complete calculating abundance on aggregated S2 scence.\n")
-    np.save('%s/s2_500m_abundance.npy' % tbd, s2_abundance_500m)
+    # print("-----------> Complete calculating abundance on aggregated S2 scence.\n")
+    # np.save('%s/s2_500m_abundance.npy' % tbd, s2_abundance_500m)
 
     # plot 2d abundance figures
-    for i in range(main_endmember.shape[0]):
-        abundane_i = s2_abundance_500m[:, :, i]
-        abundane_i = abundane_i.reshape(s2_rows_500m, s2_cols_500m)
-        abundane_i[boa_band02_500m_array.reshape((s2_rows_500m, s2_cols_500m)) < 0] = np.nan
-
-        colortable_i = ascii_uppercase[i]
-        _plot_2d_abundance(abundane_i, fig_directory, colortable_i)
+    # for i in range(main_endmember.shape[0]):
+    #     abundane_i = s2_abundance_500m[:, :, i]
+    #     abundane_i = abundane_i.reshape(s2_rows_500m, s2_cols_500m)
+    #     abundane_i[boa_band02_500m_array.reshape((s2_rows_500m, s2_cols_500m)) < 0] = np.nan
+    #
+    #     colortable_i = ascii_uppercase[i]
+    #     _plot_2d_abundance(abundane_i, fig_directory, colortable_i)
 
     # load solar and sensor angular data
     granule_dir = os.path.join(sentinel2_directory, 'GRANULE')
