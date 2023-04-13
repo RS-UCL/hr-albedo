0b9c81a91bb38c1683fc2a3be6b3ef1a4d5c1924
ruisong <rui.song@ucl.ac.uk>
2023-04-13 18:02:26 +0100

    val

---
+++ main_albedo_masaic_v2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Filename:    main_albedo_masaic_v2.py
+# @Author:      Dr. Rui Song
+# @Email:       rui.song@physics.ox.ac.uk
+# @Time:        13/04/2023 17:55
+
+import datetime
+import glob
+import yaml
+import sys
+
+# python main_albedo_masaic_v2.py ./data/S2GM_T10_20220711_20220720_s2gm-LaCrau-v2_STD_v2.0.1/tile_0/ 30 1000 100
+
+sentinel2_directory = sys.argv[1]
+sample_interval = int(sys.argv[2])
+patch_size = int(sys.argv[3])
+patch_overlap = int(sys.argv[4])
