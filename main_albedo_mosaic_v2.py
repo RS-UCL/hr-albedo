357c76a164ac92f8dd5295d593b83e8bfdb2e0c2
ruisong <rui.song@ucl.ac.uk>
2023-05-18 13:33:14 +0100

    val

--- main_albedo_mosaic_v2.py
+++ main_albedo_mosaic_v2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-# @Filename:    main_albedo_masaic_v2.py
+# @Filename:    main_albedo_mosaic_v2.py
 # @Author:      Dr. Rui Song
 # @Email:       rui.song@physics.ox.ac.uk
 # @Time:        13/04/2023 17:55
@@ -11,7 +11,7 @@ import glob
 import yaml
 import sys
 
-# python main_albedo_masaic_v2.py ./data/S2GM_T10_20220711_20220720_s2gm-LaCrau-v2_STD_v2.0.1/tile_0/ 30 1000 100
+# python main_albedo_mosaic_v2.py ./data/S2GM_T10_20220711_20220720_s2gm-LaCrau-v2_STD_v2.0.1/tile_0/ 30 1000 100
 
 # sentinel2_directory = sys.argv[1]
 # sample_interval = int(sys.argv[2])
