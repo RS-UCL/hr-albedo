284e1d8f926fa46d21f2ab0579e3283ae36d2b5f
ruisong <rui.song@ucl.ac.uk>
2023-05-18 12:20:21 +0100

    val

--- kernel/test_v2.py
+++ kernel/test_v2.py
@@ -31,7 +31,7 @@ def create_plot(data_array, description, cmap, output_dir):
 # index_dir = "/Users/rs/Projects/hr-albedo/data/Nairobi_M10_February_2022_UTM_CM_SIAC_VIIRS_NEW/S2GM_M10_20220201_20220228_Nairobi_STD_v2.0.1/tile_0/"
 # remote directory
 input_dir = '/gws/nopw/j04/qa4ecv_vol3/S2GM/hr-albedo/data/Nairobi_M10_February_2022_UTM_CM_SIAC/S2GM_M10_20220201_20220228_Nairobi_STD_v2.0.1/tile_0/VIIRS_prior/'
-index_dir = '/gws/nopw/j04/qa4ecv_vol3/S2GM/hr-albedo/data/Nairobi_M10_February_2022_UTM_CM_SIAC/S2GM_M10_20220201_20220228_Nairobi_STD_v2.0.1/'
+index_dir = '/gws/nopw/j04/qa4ecv_vol3/S2GM/hr-albedo/data/Nairobi_M10_February_2022_UTM_CM_SIAC/S2GM_M10_20220201_20220228_Nairobi_STD_v2.0.1/tile_0/'
 
 os.system(f"gdal_translate -r mode -ovr NONE -tr 500 500 {index_dir}validation_source_index.tif {index_dir}validation_source_index_500.tif")
 # index_file = index_dir + 'validation_source_index_500.tif'
